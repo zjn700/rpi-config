29a07eb5a52da705bfd6414ab4af6543cb7deb73
zjn700 <zjn700-01@yahoo.com>
2021-02-16 03:14:34 -0500

    updated on - Tue 16 Feb 2021 03:14:33 AM EST

--- sh/git-push.sh
+++ sh/git-push.sh
@@ -9,7 +9,7 @@ t="${t//:}"
 d=$d-$t
 echo $d
 cd ~/dev/config
-git status
+#git status
 set +e  # Grep succeeds with nonzero exit codes to show results.
 git status | grep 'modified\|Untracked'
 if [ $? -eq 0 ]
@@ -17,7 +17,7 @@ then
     set -e
     git add .
     git commit -am "updated on - $(date)"
-    # git push
+    git push
 else
     set -e
     echo "No changes since last run"
