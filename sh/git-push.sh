962901d2182fdef44060ed4f8f40a6f58bdb058a
zjn700 <zjn700-01@yahoo.com>
2021-02-17 03:38:22 -0500

    updated on - Wed 17 Feb 2021 03:38:22 AM EST

--- sh/git-push.sh
+++ sh/git-push.sh
@@ -9,9 +9,10 @@ echo $d
 cd ~/dev/config
 set +e  # Grep succeeds with nonzero exit codes to show results.
 git status | grep 'modified\|Untracked'
-echo $?
+# $? returns the exit value of the last executed command.
 if [ $? -eq 0 ]
 then
+  # -e  Exit immediately if a command exits with a non-zero status
     set -e
     git add .
     git commit -am "updated on - $(date)"
