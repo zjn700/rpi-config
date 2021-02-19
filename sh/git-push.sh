75dff8c5706dc64cfcfa2f013a9254d9706a7c4f
zjn700 <zjn700-01@yahoo.com>
2021-02-19 03:40:04 -0500

    updated on - Fri 19 Feb 2021 03:40:04 AM EST

--- sh/git-push.sh
+++ sh/git-push.sh
@@ -1,5 +1,8 @@
 #!/bin/bash 
 
+# set -e will cause the script to exit on any error/non-zero return value.
+# set +e is the default. if there are errors the script will continue to run
+
 echo "starting..."
 d=`date +%Y-%m-%d`
 t=`date +%R`
