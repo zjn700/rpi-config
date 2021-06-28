1bd23e33e42623ff90107d9a5f5e4366a8a4e381
zjn700 <zjn700-01@yahoo.com>
2021-06-28 09:28:11 +0800

    updated on - Mon 28 Jun 2021 09:28:11 AM +08

---
+++ sh/wifi-check.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# set -e will cause the script to exit on any error/non-zero return value.
+# set +e is the default. if there are errors the script will continue to run
+
+echo "starting..."
+d=`date +%Y-%m-%d`
+t=`date +%R`
+t="${t//:}"
+d=$d-$t
+echo $d
+#cd ~/dev/config
+#set +e  # Grep succeeds with nonzero exit codes to show results.
+#git status | grep 'modified\|Untracked'
+## $? returns the exit value of the last executed command.
+#if [ $? -eq 0 ]
+#then
+#  # -e  Exit immediately if a command exits with a non-zero status
+#    set -e
+#    git add .
+#    git commit -am "updated on - $(date)"
+#    git push
+#else
+#    set -e
+#    echo "No changes since last run"
+#fi
